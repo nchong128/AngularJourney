7fa19e391dcd2158c236b2118ae40d2b937c9950
nchong128 <nchong128@gmail.com>
2019-04-26 20:12:12 +1000

    Finish up to wildcard routing

--- Routing/routing-start/src/app/app.module.ts
+++ Routing/routing-start/src/app/app.module.ts
@@ -12,11 +12,18 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import {RouterModule, Routes} from "@angular/router";
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   {path:'', component: HomeComponent},
-  {path:'users', component: UsersComponent},
-  {path:'servers', component: ServersComponent}
+  {path:'users', component: UsersComponent, children: [
+    {path: ':id/:name', component: UserComponent},
+  ]},
+  {path:'servers', component: ServersComponent, children: [
+    {path:':id', component: ServerComponent},
+    {path:':id/edit', component: EditServerComponent}
+  ]},
+  {path: "**",  component: PageNotFoundComponent}
 ];
 
 @NgModule({
@@ -27,7 +34,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
--- Routing/routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ Routing/routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import {ActivatedRoute, Params} from "@angular/router";
 
 @Component({
   selector: 'app-edit-server',
@@ -11,10 +12,20 @@ export class EditServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
+  allowEdit = false;
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+              private  route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.queryParams.subscribe(
+      (updatedParams: Params) => {
+        this.allowEdit = updatedParams['allowEdit'] === '1';
+      }
+    );
+    this.route.fragment.subscribe();
+
+
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
@@ -23,5 +34,6 @@ export class EditServerComponent implements OnInit {
   onUpdateServer() {
     this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
   }
+
 
 }
